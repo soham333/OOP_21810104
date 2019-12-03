b94b317d69cccb212c7c13b51fd6dc4ce4e04042
soham333 <55544088+soham333@users.noreply.github.com>
2019-12-03 08:51:36 +0530

    Update ass7.cpp

--- ass7.cpp
+++ ass7.cpp
@@ -1,55 +1,116 @@
 #include<iostream>
 #include<list>
+#include<stdlib.h>
+
 using namespace std;
+
 void display(list<int> &l)
 {
-list<int>::iterator itr;
-for(itr=l.begin();itr!=l.end();++itr)
-{
-cout<<*itr<<"\t";
+	list<int>::iterator itr;
+	
+	for(itr=l.begin(); itr!=l.end(); ++itr)
+	{
+		cout<<*itr<<" ";
+	}
 }
-cout<<endl;
-}
+
 int main()
 {
-	list <int> l1;
-	list<int> l2(5);
-	int no,value;
-
-	cout<<"Enter the no of elements in list 1: ";
-	cin>>no;
-	cout<<"Enter the values"<<endl;
-	int i;
-    for(i=0;i<no;i++){
-        cin>>value;
-        l1.push_back(value);
-    }
-    display(l1);
-
-    list<int>::iterator itr2;
-    i=1;
-	for(itr2=l2.begin();itr2!=l2.end();++itr2)
+	
+		int x,pos,no,elem;
+	int value;
+	list<int>l1;
+	list<int>l2;
+	
+	for(int i=0;i<5;i++)
 	{
-		*itr2=i*2;
-		i++;
+		cout<<"ENTER THE VALUE FOR FIRST LIST"<<'\n';
+		cin>>value;
+		l1.push_back(value);
 	}
-cout<<"List 1 element are:"<<endl;
-display(l1);
-cout<<"List 2 element are:"<<endl;
-display(l2);
-list<int>listA,listB;
-listA=l1;
-listB=l2;
-l1.merge(l2);
-cout<<"Merge unsorted list:"<<endl;
-display(l1);
-listA.sort();
-listB.sort();
-listA.merge(listB);
-cout<<"Merge sorted list:"<<endl;
-display(listA);
-cout<<"Reverse list:"<<endl;
-listA.reverse();
-display(listA);
-return 0;
+	
+	system("CLS");
+	
+	for(int i=0;i<1000;i++)
+	{
+
+	cout<<"WHAT FUNCTION DO YOU WANNT TO PERFORM"<<'\n';
+	cout<<"1.DISPLAY FIRST LIST"<<'\n';
+	cout<<"2.SORT FIRST LIST"<<'\n';
+	cout<<"3.MERGE THE LISTS"<<'\n';
+	cout<<"4.CHECK IF THE LIST ARE EMPTY"<<'\n';
+	cout<<"5.INSERT ELEMENTS IN LIST"<<'\n';
+	cout<<"6.REVERSE THE LIST"<<'\n';
+	cout<<"7.REMOVE THE REPEATATING ELEMENTS IN LIST"<<'\n';
+	cout<<"8.EXIT"<<'\n';
+	
+	cin>>x;
+	
+	switch(x)
+	{
+	case 1:
+	display(l1);
+	cout<<'\n';
+	break;
+	
+	case 2:
+	l1.sort();
+	display(l1);
+	cout<<'\n';
+	break;
+	
+	case 3:
+	for(int i=0;i<5;i++)
+	{
+		cout<<"ENTER THE VALUE FOR SECOND LIST"<<'\n';
+		cin>>value;
+		l2.push_back(value);
+	}
+	
+	l1.merge(l2);
+	display(l1);
+	break;
+	
+	case 4:
+	if(l1.empty()==true)
+	{
+		cout<<"LIST IS EMPTY"<<'\n';
+	}
+	else
+	{
+		cout<<"LIST IS NOT EMPTY"<<'\n';
+	}
+	break;
+	
+	case 5:
+		
+		{
+		list<int>::iterator it1=l1.begin();
+		
+		cout<<"ENTER THE POSITION AT WHICH YOU WANT TO INSERT A NO."<<'\n';
+		cin>>pos;
+		
+		advance(it1,pos-1);
+		
+		cout<<"ENTER THE ELEMENTS"<<'\n';
+		cin>>elem;
+		
+		l1.insert(it1,elem);
+		display(l1);
+		}
+	break;	
+	case 6:
+		l1.reverse();
+		display(l1);
+		break;
+	case 7:
+		l1.unique();
+		display(l1);
+		break;
+	case 8:
+		return 0;
+	}
+}
+	
+	
 }
