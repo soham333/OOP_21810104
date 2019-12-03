41fb3999ae73afc2713bb7b87942696df23c7dff
soham333 <55544088+soham333@users.noreply.github.com>
2019-12-03 13:24:01 +0530

    Update ass1.cpp

--- ass1.cpp
+++ ass1.cpp
@@ -1,274 +1,94 @@
-#include <iostream>
-
+#include<iostream>
+#include<stdio.h>
+#include<string>
 using namespace std;
-class data;  
-class student
+class emp
 {
-    string name;
-    string branch;
-    char* div;
-    int date;
-    int month;
-    int year;
-
-    string bg;
-    static int count;  //Static members
-	public:
-    	int roll_no;
-    	student()       //Constructor
-    	{
-        	name="";
-        	roll_no = 0;
-        	branch = "";
-        	div = new char;
-       		date;
-       		month;
-       		year;
-       		 bg = new char[4];
-    	}
-    	
-    static int getcount()
+   unsigned int mo,da,ye;
+    string na,add;
+    string idd;
+    long long phno;
+    string bg,bgk;
+    double sal;
+public:
+    emp()
     {
-        return count;
+
     }
-    
-    void getData(data*);
-    void dispData(data*);
-
+    void accept();
+    void display();
 };
-
-class data
+void emp::accept()
 {
-    string caddress;
-    long long int *teleno;
-    long long int *dlno;
-    friend class student;    //Friend Class of data
-	public:
-	    data()
-	    {	
-	        caddress = "";
-        	teleno = new long long int;   //Dynamic Memory Allocation
-        	dlno = new long long int;
-    	}	
-    
-    	void getdata()
-    	{
-        	cout<<"Enter Contact Address"<<endl;
-        	cin.get();
-        	getline(cin,caddress);
-        	while(1)
-        	{
-        	cout<<"Enter the Telephone Number"<<endl;
-        	cin>>*teleno;
-        	if(*teleno<1000000000||*teleno>9999999999)
-        	{
-        	  cout<<"Please Enter a valid telephone Number"<<endl;
-        	}
-        	else
-        	break;
-        	}
-        	cout<<"Enter the Driving License number:"<<endl;
-       		cin>>*dlno;
-    	}
-    	void dispStudData()
-    	{
-	        cout<<"Contact Address : "<<caddress<<endl;
-	        cout<<"Telephone Number : "<<*teleno<<endl;
-	        cout<<"Driving License Number : "<<*dlno<<endl;	
-	    }
-
-};
-
-inline void student:: getData(data* st)   //Inline Function
-{
-	int x,b,u,a;
-    cout<<"Enter Student Name: ";
-    cin.get();
-    getline(cin,name);
-    cout<<"Enter Roll Number: ";
-    cin>>roll_no;
-    cout<<"Enter Branch: ";
-    cin.get();
-    getline(cin,branch);
-    cout<<"Enter the div: ";
-    cin>>div;
-    x:cout<<"\nEnter the month for Date of Birth: ";
-    cin>>month;
-   	if(month<1||month>12)
-  	 {
-       cout<<"Invalid Month"<<endl;
-       goto x;
-     }
-    else
-    	b:cout<<"Enter the Date for Date of Birth: ";
-    	cin>>date;
-    	if(month==2&&date>29)
-    	{
-    	    cout<<"Invalid Date"<<endl;
-    	    goto b;
-    	}
-    	else if((month==4||6||9||11)&&date>30)
-    	{
-    	    cout<<"Invalid Date"<<endl;
-    	    goto b;
-    	}
-    	else if((month==1||3||5||7||8||10||12)&&date>31)
-    	{
-    	    cout<<"Invalid Date"<<endl;
-    	    goto b;
-    	}
-    	else
-		    u:cout<<"\nEnter the year for Date of Birth: ";
-    		cin>>year;
-    		if(year>2019)
-    		{
-          		cout<<"Invalid year"<<endl;
-          		goto u;
-    		}
-   		a:
-    cout<<"choose blood group from below\n";
-    cout<<"1-A+\n";
-    cout<<"2-B+\n";
-    cout<<"3-AB+\n";
-    cout<<"4-O+\n";
-    cout<<"5-A-\n";
-    cout<<"6-B-\n";
-    cout<<"7-AB-\n";
-    cout<<"8-O-\n";
-    int choice;
-    cin>>choice;
-	cout<<endl;
-    switch(choice)
+string bg1[9]={"A+","A-","B+","B-","AB+","AB-","O+","O-","\0"};
+    int k=0,g=1;
+    cout<<"Input Id:\n";
+    cin.ignore();
+    getline(cin,idd);
+    cout<<"Input Name :\n";
+    getline(cin,na);
+    cout<<"Input Address :\n";
+    getline(cin,add);
+    cout<<"Enter Dob :\n";
+    cout<<"Day :\n";
+    cin>>da;
+    cout<<"Month :\n";
+    cin>>mo;
+    cout<<"Year :\n";
+    cin>>ye;
+    cout<<"\n";
+    if(da>31)
+        cout<<"Invalid Day Entered\n";
+        if(da<1)
+            cout<<"Invalid Day Entered\n";
+        if(mo<1)
+        cout<<"Invalid Month Entered\n";
+        if(mo>12)
+            cout<<"Invalid Month Entered\n";
+        if(ye<1)
+            cout<<"Invalid Year Entered\n";
+    cout<<"Enter Salary :\n";
+    cin>>sal;
+    cout<<"Enter BloodGroup :\n";
+    cin.ignore();
+    getline(cin,bg);
+    for(k=0;bg1[k]!="\0";k++)
     {
-        case 1:
-       		bg="A+";
-       		break;
-        case 2:
-        	bg="B+";
-        	break;
-        case 3:
-        	bg="AB+";
-        	break;
-        case 4:
-        	bg="O+";
-        	break;
-        case 5:
-        	bg="A-";
-        	break;
-        case 6:
-        	bg="B-";
-        	break;
-        case 7:
-        	bg="AB-";
-        	break;
-        case 8:
-        	bg="O-";
-        	break;
-        default:
-        	cout<<"enter the correct choice again\n";
-        	goto a;
-   	}
-    count++;
+        if(bg==bg1[k])
+        {
+        g=0;
+        if(g==0)
+        {
+            bgk=bg1[k];
+        }
+        }
+    }
+
 }
-
-
- inline void student::dispData(data* st1)  //Inline Function
+void emp::display()
 {
-
-    cout<<"Student Name: "<<this->name<<endl;     //this pointer
-    cout<<"Roll Number: "<<this->roll_no<<endl;
-    cout<<"Branch: "<<this->branch<<endl;
-    cout<<"div: "<<this->div<<endl;
-    cout<<"Date of Birth: "<<date<<"/"<<month<<"/"<<year<<endl;
-    cout<<"Blood Group: "<<this->bg<<endl;
-    st1->dispStudData();
-	cout<<"\n\n";
+    cout<<"\nId is :"<<idd<<endl;
+    cout<<"Name is :"<<na<<endl;
+    cout<<"Address is :"<<add<<endl;
+    cout<<"Date of Birth is :"<<da<<"/"<<mo<<"/"<<ye<<endl;
+    cout<<"Salary is :"<<sal<<endl;
+     cout<<"Blood Group is:"<<bgk<<"\n";
+     cout<<"ENTER VALID BLOOD GROUP \n";
+   
 }
-int student::count;
-
-
 int main()
 {
-    student* stud1[100];
-    data* stud2[100];
-    int n=0,x,i,roll;
-    int flag=0;
-	do
+    int n;
+    cout<<"Input Number of Employees : \n";
+    cin>>n;
+    emp obj[n];
+    for(int i=0;i<n;i++)
     {
-    	 cout<<" 1.Add Record \n 2.Display Records \n 3.Delete Records \n 4.Modify \n 5.Exit "<<endl<<endl;
-    	 cin>>x;
-    	 cin.get();
-    	 switch(x)
-    	 {
-    	  case 1:
-    		    stud1[n]=new student;  //Dynamic Memory Allocation
-    		    stud2[n]=new data;  //Dynamic Memory Allocation
-    		    stud1[n]->getData(stud2[n]);
-    		    stud2[n]->getdata();
-    		    n++;
-    		    cout<<"Record Added Successfully"<<endl<<endl;
-    			break;
-	      case 2:
-    	   		if(n==0)
-          		{
-              		cout<<"DATABASE IS EMPTY"<<endl;
-          		}
-          		else
-          		{
-		        	for(i=0;i<n;i++)
-		          	{
-		            	stud1[i]->dispData(stud2[i]);
-		          	}
-          		}
-	      		break;
-	      case 3:
-    	        if(n<1)
-    	        {
-    	          cout<<"DATABASE IS EMPTY"<<endl;
-        	    }
-        	    else
-        	    {
-	        	    cout<<"Enter the Roll number of Student to be Deleted"<<endl;
-    		        cin>>roll;
-    		        for(i=0;i<n;i++)
-    		        {
-    		            if(stud1[i]->roll_no == roll)
-    		            {
-    		                while(i<n-1)
-    		                {
-    			                 stud1[i] = stud1[i+1];
-    			                 stud2[i] = stud2[i+1];
-    			                 i++;
-    		                }
-    		                n--;
-		                }
-        		        else
-        		        {
-                		    cout<<"RECORD  DOES NOT EXIST"<<endl<<endl;
-                		}
-            		}	
-            		cout<<"Record Deleted Successfully"<<endl<<endl;
-            	}
-            	break;
-            case 4:
-            		cout<<"Enter the Roll number of Student to be modified"<<endl;
-    		        cin>>roll;
-    		        for(i=0;i<n;i++)
-    		        {
-    		            if(stud1[i]->roll_no == roll)
-    		            {
-    		            stud1[i]->getData(stud2[i]);
-    		  		    stud2[i]->getdata();
-    		  		    flag=1;
-    		            }
-    		          }
-    		          if(flag==0)
-    		            	cout<<"Record Not available"<<endl;
-    		           break;
-    		 case 5:
-    		 		break;     
-    		         
-     	}
-    }while(x!=5);
+        obj[i].accept();
+    }
+    for(int j=0;j<n;j++)
+    {
+        obj[j].display();
+    }
     return 0;
 }
